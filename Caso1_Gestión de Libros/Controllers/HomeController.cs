7a4426f25c7e008056efa620d588a0a57d912e94
maryammf2 <fuentesmaryam2@gmail.com>
2025-11-11 20:40:11 -0600

    Creación de proyecto vacío

---
+++ Caso1_Gestión de Libros/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Caso1_Gestión_de_Libros.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Caso1_Gestión_de_Libros.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Caso1_Gestión de Libros/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Caso1_Gestión_de_Libros.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
