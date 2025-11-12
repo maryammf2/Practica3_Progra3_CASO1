ea432187609f47e9da11f9142bc245d3f10bbe79
maryammf2 <fuentesmaryam2@gmail.com>
2025-11-11 23:00:59 -0600

    Eliminar carpeta Caso1_Gestión de Libros

--- Caso1_Gestión de Libros/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Caso1_Gestión_de_Libros.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
