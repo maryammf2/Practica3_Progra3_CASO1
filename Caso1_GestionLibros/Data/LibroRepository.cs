df44eef74dd795125db54f2d571cfbdeb548b82b
maryammf2 <fuentesmaryam2@gmail.com>
2025-11-17 21:39:51 -0600

    Fase 3 – Creación del Repositorio y cambio en la clase Libro

---
+++ Caso1_GestionLibros/Data/LibroRepository.cs
@@ -0,0 +1,26 @@
+﻿using Caso1_GestionLibros.Models;
+
+namespace Caso1_GestionLibros.Data
+{
+    public class LibroRepository
+    {
+        private static List<Libros> _libros = new List<Libros>();
+
+        public void AgregarLibro(Libros libro)
+        {
+            _libros.Add(libro);
+        }
+
+        public List<Libros> ObtenerLibros()
+        {
+            return _libros;
+        }
+
+        public bool ExisteCodigoInterno(string codigoInterno)
+        {
+            return _libros.Any(l => l.CodigoInterno.Equals(codigoInterno, StringComparison.OrdinalIgnoreCase));
+        }
+
+
+    } //class
+} //end
--- Caso1_GestionLibros/Models/Libros.cs
+++ Caso1_GestionLibros/Models/Libros.cs
@@ -4,30 +4,30 @@ namespace Caso1_GestionLibros.Models
 {
     public class Libros
     {
-            [Required(ErrorMessage = "El título es obligatorio.")]
-            [StringLength(200, MinimumLength = 3, ErrorMessage = "El título debe tener entre 3 y 200 caracteres.")]
-            public string Titulo { get; set; } = string.Empty;
+        [Required(ErrorMessage = "El título es obligatorio.")]
+        [StringLength(200, MinimumLength = 3, ErrorMessage = "El título debe tener entre 3 y 200 caracteres.")]
+        public string Titulo { get; set; } = string.Empty;
 
-            [Required(ErrorMessage = "El autor es obligatorio.")]
-            [StringLength(100, MinimumLength = 3, ErrorMessage = "El autor debe tener entre 3 y 100 caracteres.")]
-            public string Autor { get; set; } = string.Empty;
+        [Required(ErrorMessage = "El autor es obligatorio.")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "El autor debe tener entre 3 y 100 caracteres.")]
+        public string Autor { get; set; } = string.Empty;
 
-            [Required(ErrorMessage = "La categoría es obligatoria.")]
-            public string Categoria { get; set; } = string.Empty;
+        [Required(ErrorMessage = "La categoría es obligatoria.")]
+        public string Categoria { get; set; } = string.Empty;
 
-            [Required(ErrorMessage = "El año de publicación es obligatorio.")]
-            [Range(1900, 2025, ErrorMessage = "El año debe estar entre 1900 y el año actual.")]
-            public int AnioPublicacion { get; set; }
+        [Required(ErrorMessage = "El año de publicación es obligatorio.")]
+        [Range(1900, 2025, ErrorMessage = "El año debe estar entre 1900 y el año actual.")]
+        public int AnioPublicacion { get; set; }
 
-            [Required(ErrorMessage = "El número de páginas es obligatorio.")]
-            [Range(1, int.MaxValue, ErrorMessage = "El número de páginas debe ser mayor que 0.")]
-            public int NumeroPaginas { get; set; }
+        [Required(ErrorMessage = "El número de páginas es obligatorio.")]
+        [Range(1, int.MaxValue, ErrorMessage = "El número de páginas debe ser mayor que 0.")]
+        public int NumeroPaginas { get; set; }
 
-            [Required(ErrorMessage = "El código interno es obligatorio.")]
-            [RegularExpression(@"^LIB-\d{3,}$", ErrorMessage = "El código debe seguir el formato LIB-### (ej. LIB-001).")]
-            public string CodigoInterno { get; set; } = string.Empty;
+        [Required(ErrorMessage = "El código interno es obligatorio.")]
+        [RegularExpression(@"^LIB-\d{3,}$", ErrorMessage = "El código debe seguir el formato LIB-### (ej. LIB-001).")]
+        public string CodigoInterno { get; set; } = string.Empty;
 
-            [Required(ErrorMessage = "La disponibilidad es obligatoria.")]
-            public bool Disponible { get; set; }
-} //class
+        [Required(ErrorMessage = "La disponibilidad es obligatoria.")]
+        public bool Disponible { get; set; }
+    } //class
 } //end
